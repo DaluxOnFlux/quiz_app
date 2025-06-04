c2f7a119b9655b83f31109b3c861ba7421ef5f72
smiranii <ichrak.smirani@edu.esiee.fr>
2025-06-04 10:10:08 +0200

    API flask Hello World

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_cors import CORS  # Import du package
 
 app = Flask(__name__)
+CORS(app)  # Activation de CORS sur l'application Flask
 
 @app.route('/')
 def hello_world():
-	x = 'world'
-	return f"Hello, {x}"
+    x = 'world'
+    return f"Hello, {x}"
 
 if __name__ == "__main__":
     app.run()
