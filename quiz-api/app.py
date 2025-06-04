c46a44a1a681d3f9662b9ce9264ba2297b8dde29
DaluxOnFlux <dalil.hiane@edu.esiee.fr>
2025-06-04 11:29:21 +0200

    Add Login/Pwd

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,5 +1,8 @@
+import hashlib
 from flask import Flask
 from flask_cors import CORS  # Import du package
+from flask import Flask, request
+from jwt_utils import build_token, decode_token, JwtError
 
 app = Flask(__name__)
 CORS(app)  # Activation de CORS sur l'application Flask
@@ -12,6 +15,20 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+ADMIN_PASSWORD = "flask2023"  # à adapter selon le mot de passe à tester dans Postman
+
+@app.route('/login', methods=['POST'])
+def login():
+    body = request.get_json()
+    if not body or 'password' not in body:
+        return {"error": "Mot de pass requis"}, 400
+
+    if body['password'] != ADMIN_PASSWORD:
+        return {"error": "Mot de pass incorrect"}, 401
+
+    token = build_token()
+    return {"token": token}, 200
 
 if __name__ == "__main__":
     app.run()
